6b77be3a25492af69efa8b101a00272f0671af12
Hiztree <owner.prestige@gmail.com>
2015-01-29 15:36:34 -0700

    Added sendMessage

--- me/BlazingCode/TheBasics/Methods/Methods.java
+++ me/BlazingCode/TheBasics/Methods/Methods.java
@@ -1,5 +1,10 @@
 package me.BlazingCode.TheBasics.Methods;
 
-public class Methods {
-
+public class Methods 
+{
+  String prefix = ChatColor.BLACK + "[" + ChatColor.GOLD + "TheBasics" + ChatColor.BLACK + "] " + ChatColor.GOLD;
+  public static void sendMessage(Player p, String m)
+  {
+    p.sendMessage(prefix + m);
+  }
 }
