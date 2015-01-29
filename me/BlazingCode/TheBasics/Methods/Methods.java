e80a23ea5bc26c65a5bc23b6d99588429a9a6412
Hiztree <owner.prestige@gmail.com>
2015-01-29 15:40:32 -0700

    Fixed SendMessage

--- me/BlazingCode/TheBasics/Methods/Methods.java
+++ me/BlazingCode/TheBasics/Methods/Methods.java
@@ -1,11 +1,14 @@
 package me.BlazingCode.TheBasics.Methods;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 public class Methods 
 {
-  String prefix = ChatColor.BLACK + "[" + ChatColor.GOLD + "TheBasics" + ChatColor.BLACK + "] " + ChatColor.GOLD;
-  
-  public static void sendMessage(Player p, String m)
-  {
-    p.sendMessage(prefix + m);
-  }
+	  private static String prefix = ChatColor.BLACK + "[" + ChatColor.GOLD + "TheBasics" + ChatColor.BLACK + "] " + ChatColor.GOLD;
+	  
+	  public static void sendMessage(Player p, String m)
+	  {
+	    p.sendMessage(prefix + m);
+	  }
 }
